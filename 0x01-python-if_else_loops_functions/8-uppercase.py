9b85f519729f031491e329b8e2f843eb6af971e3
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-10 20:49:43 +0000

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        if (ord(ch) >= ord('a')) and (ord(ch) <= ord('z')):
+                ch = chr(ord(ch) - 32)
+        print("{:s}".format(ch), end="")
+    print("")
+
