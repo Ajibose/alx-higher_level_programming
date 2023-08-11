47eef275333a5ef00901e34cd714f3ca8307ac40
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-11 12:19:40 +0000

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
     size = len(sys.argv)
-    if (size == 2):
+    if(size == 2):
         print ("{:d} argument:".format(size - 1))
-    elif (size == 1):
+    elif(size == 1):
         print ("{:d} arguments.".format(size - 1))
-    elif (size > 2):
+    elif(size > 2):
         print ("{:d} arguments:".format(size - 1))
     for i in range(1, size):
         print("{:d}: {:s}".format(i, sys.argv[i]))
