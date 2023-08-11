b90822df7bd3c7593ea16613dd85acf292964411
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-11 16:02:24 +0000

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    name_list = dir(hidden_4)
+    for name in name_list:
+        if name.startswith("__"):
+            continue
+        print("{:s}".format(name))
