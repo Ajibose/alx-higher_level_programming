cec568790ea8880d31a7a730c9cd18a279193d55
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-12 17:03:11 +0000

    Task4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 
 def new_in_list(my_list, idx, element):
     """
-    Replaces the element at position idx of my_list not modifying the original list
+    Replaces the element at position idx of
+     my_list not modifying the original list
 
     Args:
         my_list(list): list to modify
