4f0bf523373d214073dfb46c9477df86375c94d1
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-14 13:27:43 +0000

    Task6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -11,9 +11,12 @@ def print_matrix_integer(matrix=[[]]):
         return None
     """
     if matrix is None:
-        return None
+        print("")
 
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=" ")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
         print("")
