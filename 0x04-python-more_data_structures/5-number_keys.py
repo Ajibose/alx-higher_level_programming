8096a17a0ccb594a40ad8fdc02b997551dd0dca2
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-16 17:18:53 +0000

    Task5. Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """returns the number of keys in a dictionary."""
+    i = 0
+    for key in a_dictionary.keys():
+        i += 1
+    return (i)
