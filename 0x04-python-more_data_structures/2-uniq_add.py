0c332c2de0932a2c28482ed94aaabb3525441124
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-16 17:03:28 +0000

    Task2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list (only once for each integer).
+
+    Args:
+        my_list(list): list to sum
+
+    Return:
+        return sum of all element in my_list
+    """
+    total = __import__('functools').reduce(lambda x, y: x + y, set(my_list))
+    return (total)
