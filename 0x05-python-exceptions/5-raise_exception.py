23739d9f756b2bc39c29ac2312b999bc9dd871cd
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-08-28 18:51:01 +0000

    raises exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,5 +2,5 @@
 
 def raise_exception():
     """raises a type exception"""
-    
+
     raise TypeError
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,5 +2,5 @@
 
 def raise_exception_msg(message=""):
     """raises a name exception with a message."""
- 
+
     raise NameError(message)
