32a13aa9dde738eee7314ded31c8c7cedba97805
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-05 19:20:03 +0000

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,6 +5,6 @@
 class LockedClass():
     def __setattr__(self, name, value):
         if name != "first_name":
-            raise AttributeError(f"'{type(
-                    self).__name__}' object has no attribute '{name}'")
+            raise AttributeError(
+                    f"'{type(self).__name__}' object has no attribute '{name}'")
         super().__setattr__(name, value)
