9cef2a8429edeac1addda453c05fb6f4b9adf5bc
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-06 14:01:47 +0000

    Task0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Integers addition"""
+"""
+    Defines a function that add two integers
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two numbers
-    Args:
-        a - first number input
-        b - second number input
+    """Adds two integers"""
 
-    """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
