b93d836eaf5b032332fa001481057cdae94044d9
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-06 15:10:44 +0000

    Task1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,18 +6,24 @@ def matrix_divided(matrix, div):
     """Divide all elements of matrix by div"""
 
     if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats"
+                        )
 
     for lists in matrix:
         if not isinstance(lists, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                            "(list of lists) of integers/floats"
+                            )
         size = len(matrix[0])
         if len(lists) != size:
             raise TypeError("Each row of the matrix must have the same size")
-        
+
         for i in lists:
             if not isinstance(i, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                                "(list of lists) of integers/floats"
+                                )
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
