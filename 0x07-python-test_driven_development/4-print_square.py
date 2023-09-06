c47fbe4667d1cc8a590bed2c4ace306e7bfc3980
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-06 16:52:52 +0000

    Task3. Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,13 +3,14 @@
 
 
 def print_square(size):
-    """Prints a square with the character '#'"""
-    if type(size) != int:
+    """Prints a square with the symbol '#'"""
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    for x in range(size):
-        for y in range(size):
+
+    for i in range(size):
+        for j in range(size):
             print("#", end='')
-        print()
-        
+        print()        
