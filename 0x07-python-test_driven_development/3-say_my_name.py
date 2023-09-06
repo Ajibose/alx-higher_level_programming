b4bce8e66089878eb5333c5ba8e4ba8592912053
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-06 16:20:14 +0000

    Task2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,13 +3,9 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string.
-    Validates string inputs, the display
-    the full name
-
-    """
-    if type(first_name) != str:
-        raise TypeError("first name must be a string")
-    if type(last_name) != str:
-        raise TypeError("last name must be a string")
+    """Prints a string """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
