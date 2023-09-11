f002bc0b718f7fb7f3d81e68d9fdf86eceda6bc6
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-11 16:16:00 +0000

    Task0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Get the all attribute and methods of an object
+
+    Return:
+        Return the list of the attriutes and methods
+
+    """
+    if not obj:
+        return
+
+    return dir(obj)
