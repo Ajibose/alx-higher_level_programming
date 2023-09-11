fe8452fb8290090c9b71ce166379ac9819d7f3e0
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-11 21:04:40 +0000

    Advanced task13. Can I?

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""101-add_attribute.py"""
+
+
+def add_attribute(obj, name, value):
+    """adds a new attribute to an object if its possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    obj.name = value
