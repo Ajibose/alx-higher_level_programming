8749de44834f1818ff15d239bc342ab8c8b9cd73
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-11 19:54:27 +0000

    Task9. Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class that inherits from another class based on 8-rectangle.py"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from BaseGeometry (8-base_geometry.py)."""
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Compute the area"""
+        return self.__width * self.__height
