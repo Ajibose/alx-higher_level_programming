3ab4f4bbf77d832d80806e366f21f6b6c0bdd836
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-11 18:18:57 +0000

    Task6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class based on 5-base_geometry.py"""
+
+
+class BaseGeometry():
+    """A class that defines a method"""
+    def area(self):
+        """A method that raises an exception"""
+        raise Exception("area() is not implemented")
