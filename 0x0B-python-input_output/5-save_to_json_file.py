4b8d81919f38c364b27bb396263b5b0cee914ac8
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-13 10:06:36 +0000

    Task7. Load, add, save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,5 +13,5 @@ def save_to_json_file(my_obj, filename):
         filename: path to file to write to
 
     """
-    with open(filename, 'a', encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
