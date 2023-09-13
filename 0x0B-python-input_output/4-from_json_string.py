5f2332af0377a8b60909305c26555e2268c6ebe0
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-13 09:03:59 +0000

    Task4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Deserialize a python data object"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Gets an object (Python data structure) represented by a JSON string:
+
+    Args:
+        my_str: the object to serialize
+
+    Return:
+        returns pytho data representation of my_str
+
+    """
+    return json.loads(my_obj)
