c4271360c4e9667f2de3b0292b8833f785484f8d
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-13 08:54:40 +0000

    Task2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#/usr/bin/python3
+"""Defines a function that appends to the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """Appends to the end of a file using UTF8 encoding
+
+    Args:
+        filename(str, optional): path to file to write to 
+        text(str, optional): string to append to file
+
+    Return:
+        returns the number of character written to filename
+
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        ch = f.write(text)
+
+    return ch
