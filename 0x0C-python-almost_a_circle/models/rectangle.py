2d8960cf512e05c4c404380cf4873d11b943ac88
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-18 18:44:36 +0000

    Project model classes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(Base):
     def __str__(self):
         """Rectagle str representation"""
         return f"[{self.__class__.__name__}] ({self.id})"\
-            f"{self.__x}/{self.__y} - {self.__width}/{self.__height}"
+                f"{self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
     @property
     def width(self):
