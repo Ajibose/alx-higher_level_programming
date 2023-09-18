36740563fc0fa807e8a7eba5f20653260b84a8d8
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-09-18 19:55:06 +0000

    Project model classes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,7 +33,7 @@ class Base():
     def to_json_string(list_dictionaries):
         """Return the json string representation of an object"""
 
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
 
         return json.dumps(list_dictionaries)
