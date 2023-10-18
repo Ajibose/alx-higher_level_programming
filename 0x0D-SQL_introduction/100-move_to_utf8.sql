729f691e604d27504dede3094abedd1564fcbdd4
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-10-18 22:26:03 +0000

    Advanced task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,6 +9,6 @@ ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+--ALTER TABLE first_table
+--CHARACTER SET utf8mb4
+--COLLATE utf8mb4_unicode_ci;
