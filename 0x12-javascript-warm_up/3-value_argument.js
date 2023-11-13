5315c3a4f47bc3d4fc2055a8772f036f76520d3a
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-11-13 17:24:59 +0000

    Task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv;
-if (args.length < 3) {
-  console.log('No argument');
-} else {
+if (typeof args[2] !== 'undefined') {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
