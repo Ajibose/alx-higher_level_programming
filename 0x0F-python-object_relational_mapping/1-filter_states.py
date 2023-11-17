844768c3de08b662f19a668169b2298a9410523a
Ibrahim Ajibose <ajiboseibrahim12@gmail.com>
2023-11-17 13:42:37 +0000

    make files executable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,16 +5,14 @@
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
     conn = MySQLdb.connect(
             host='localhost', user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306
-            )
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
 
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,16 +5,15 @@
 import MySQLdb
 import sys
 
-
 if __name__ == '__main__':
     conn = MySQLdb.connect(
             host='localhost', user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306
-            )
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM cities JOIN states ORDER BY id")
+
     for row in cur.fetchall():
         print(row)
 
