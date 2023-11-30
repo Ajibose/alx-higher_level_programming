981a7b953817792dd77e4b3e70b95e8645909131
Ajibose <ajiboseibrahim12@gmail.com>
2023-11-30 16:55:03 +0000

    Task3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept.
-curl -s -I -X OPTIONS "$URL" | grep -i "allow" | tr -d '\r' | awk '{print "Allowed HTTP methods:", $2}'
+curl -s -I -X OPTIONS "$URL" | grep -i "allow" | tr -d '\r' | awk "{$2}"
