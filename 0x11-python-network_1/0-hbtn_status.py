89d678dcb738c412815bca8e10e168e66574a8bd
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-01 15:54:13 +0000

    Task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Fetches the content of a webpage
+"""
+import urllib.request
+
+
+def fetch_content(url):
+    """Fetches alx-intranet.hbtn.io content"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        return response.read()
+
+
+if __name__ == '__main__'
+    fetch_content('https://alx-intranet.hbtn.io/status')
+
