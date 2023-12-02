0fe8786fb59511093867f1cad9b97a064e0c1013
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-02 03:21:37 +0000

    Task1

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    sends a post request to a url
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+def post_content(url, email):
+    """post email to url"""
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        return response.read
+
+
+
+if __name__ == '__main__':
+    post_content(sys.argv[1], sys.argv[2])
