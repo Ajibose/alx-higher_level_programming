35086fc132393b380360e67f4c026f3ff38b93f5
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-02 03:59:23 +0000

    Task5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Sends a request to a url and
+    displays the value of the variable X-Request-Id in the response header
+"""
+import requests
+import sys
+
+
+def make_connection(url):
+    """fetches https://alx-intranet.hbtn.io/status"""
+    req = requests.get(url)
+    return req.headers
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    headers = make_connection(url)
+    print(headers['X-Request-Id'])
