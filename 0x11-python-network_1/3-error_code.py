509c1ff7c9d4a0c561834f5eac5a1d253897c52e
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-02 03:37:35 +0000

    Task2

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    sends a request to the URL and displays the body of the response
+"""
+import urllib.request
+import sys
+
+
+def connect_with(url):
+    """sends a request to the URL and displays the body of the response"""
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            return response.read()
+    except urllib.error.HTTPError as e:
+        return f"Error code: {e.code}"
+
+
+if __name__ == '__main__':
+    connect_with(sys.argv[1])
