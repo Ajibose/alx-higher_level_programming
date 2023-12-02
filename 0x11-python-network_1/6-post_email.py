87465101f2cb65d01d5cc0eec20e61701e20b2c7
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-02 14:00:49 +0000

    Task6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -15,4 +15,4 @@ def post_content(url, email):
 
 
 if __name__ == '__main__':
-    print(f"Your email is: {post_content(sys.argv[1], sys.argv[2])}")
+    print(f"Your email is: {post_content(sys.argv[1], sys.argv[2]).text}")
