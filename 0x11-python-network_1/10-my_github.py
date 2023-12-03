d5412baaf4b072b4ae7a5e89f8e995f44833efd3
Ajibose <ajiboseibrahim12@gmail.com>
2023-12-03 13:35:43 +0000

    Task9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Display my id using the Github API
+"""
+import sys, requests
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    headers = {"Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {sys.argv[2]}",
+            "X-GitHub-Api-Version": "2022-11-28"}
+
+    url = f"https://api.github.com/{user}"
+    response = requests.get(url, headers=headers)
+    print(response.json["id"])
